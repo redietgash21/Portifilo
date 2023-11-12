e56fe7342fb8e488065409abf72e773dabb95fba
redietgash21 <redietgash21@gmail.com>
2023-11-12 22:21:58 +0300

    client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+import { useEffect } from 'react';
+
+import './App.css';
+// import { editTodoTable } from '../../server/controllers/todoController';
+
+
+function App() {
+  
+    useEffect(() => {
+ 
+    },[])
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
