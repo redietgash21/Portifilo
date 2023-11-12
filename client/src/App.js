64113a392d198e30aa147fb42ce3a3351db3c9cf
redietgash21 <redietgash21@gmail.com>
2023-11-12 22:34:30 +0300

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,20 @@ function App() {
     },[])
   return (
     <div className="App">
-       
+      <div className='container'>
+        <nav id="desktopNav">
+          <div class="logo">Rediet Gashaw</div>
+          <div >
+            <ul class="navLinks">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#exprience">Exprience</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>   
     </div>
   );
 }
