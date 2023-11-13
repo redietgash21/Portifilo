0307a8a0b1c5e32a3a9798b1dbb8ac8107626a17
redietgash21 <redietgash21@gmail.com>
2023-11-13 22:05:33 +0300

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -92,6 +92,119 @@ function App() {
              class="icon arrow"
              onclick="location.href='./#exprience'"/>      
     </section>
+    <section id="exprience">
+        <p class="sectionTextp1">Explore My</p>
+        <h1 class="title">Experience</h1>
+        <div class="exprianceDetailsContainer">           
+            <div class="aboutContainers">
+               <div class="detailsContainer">
+                    <h2 class="experienceSubTitle">Frontend Development</h2>
+                    <div class="articleContainer">
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                 class="icon"/>
+                                <div>
+                                    <h3>HTML</h3>
+                                    <p>Exprienced</p>
+                                </div>
+                        </article>
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                class="icon"/>
+                                <div>
+                                    <h3>CSS</h3>
+                                    <p>Exprienced</p>
+                                </div>
+                        </article>
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                class="icon"/>
+                                <div>
+                                    <h3>SASS</h3>
+                                    <p>Intermediate</p>
+                                </div>
+                        </article>
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                class="icon"/>
+                                <div>
+                                    <h3>JavaScript</h3>
+                                    <p>Basic</p>
+                                </div>
+                        </article>
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                class="icon"/>
+                                <div>
+                                    <h3>TypeScript</h3>
+                                    <p>Basic</p>
+                                </div>
+                        </article>
+                        <article>
+                            <img src="./assets/cheackMark.png" 
+                                alt="Experience Icon"
+                                class="icon"/>
+                                <div>
+                                    <h3>Material UI</h3>
+                                    <p>Intermediate</p>
+                                </div>
+                        </article>
+                    </div>
+               </div>     
+               <div class="detailsContainer">
+                <h2 class="experienceSubTitle">Backend Development</h2>
+                <div class="articleContainer">
+                    <article>
+                        <img src="./assets/cheackMark.png" 
+                            alt="Experience Icon"
+                            class="icon"/>
+                            <div>
+                                <h3>MySQL</h3>
+                                <p>Exprienced</p>
+                            </div>
+                    </article>
+                    <article>
+                        <img src="./assets/cheackMark.png" 
+                            alt="Experience Icon"
+                            class="icon"/>
+                            <div>
+                                <h3>Node JS</h3>
+                                <p>Intermediate</p>
+                            </div>
+                    </article>
+                    <article>
+                        <img src="./assets/cheackMark.png" 
+                            alt="Experience Icon"
+                            class="icon"/>
+                            <div>
+                                <h3>Express JS</h3>
+                                <p>Intermediate</p>
+                            </div>
+                    </article>
+                    <article>
+                        <img src="./assets/cheackMark.png" 
+                            alt="Experience Icon"
+                            class="icon"/>
+                            <div>
+                                <h3>Git</h3>
+                                <p>Intermediate</p>
+                            </div>
+                    </article>                         
+                </div>
+           </div>                    
+           </div>
+        </div>      
+    </div>
+    <img src="./assets/downArrow.png" 
+    alt="Down Arrow"
+    class="icon arrow"
+    onclick="location.href='./#projects'"> 
+    </section>
       </div>   
     </div>
   );
