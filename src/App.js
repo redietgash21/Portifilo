c3eaab2e17b4afeb45ef4a0e151ba932252fb2ab
redietgash21 <redietgash21@gmail.com>
2024-01-17 05:35:21 +0300

    portifilo

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#exprience">Exprience</a></li>
-              <li><a href="#projects">Projects</a></li>
+             
               <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
@@ -402,7 +402,7 @@ function App() {
     <p class="p1">To Find</p>
         <h1 class="title">Contactme On</h1>
         
-        <div class="exprianceDetailsContainer">           
+        <div class="contactDetailsContainer">           
             <div class="aboutContainers">
             <div class="detailsContainer">
                     <h3 >contact info</h3>
@@ -455,9 +455,10 @@ function App() {
         <nav>
             <div class="navLinksContainer">
                 <ul class="navLinks">
+                <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#exprience">Exprience</a></li>
-                    <li><a href="#projects">Projects</a></li>
+                    
                     <li><a href="#contact">Contact</a></li>
                 </ul>   
             </div>
