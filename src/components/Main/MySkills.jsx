2ff7d13f0e712fa17fd46712ed168091ef072cca
redietgash21 <redietgash21@gmail.com>
2024-04-08 20:54:02 +0300

    p

--- src/components/Main/MySkills.jsx
+++ src/components/Main/MySkills.jsx
@@ -13,16 +13,146 @@ function MySkills(){
         <>
      <section id="skills">
         <h2>My Skills</h2>
-          <div className="grid">
+            <div className="grid">
                 <div>
-                    <button>Front End</button>
-                    <button>Back End</button>
-                    <button>Tools</button>
-                    <button>Soft Skills</button>
+                    <h3>Front End</h3>
+                    <div className="felx">
+                       <p>HTML5</p>
+                       <div className="percentile">
+                         <div className="percentileresult eighty">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>CSS3</p>
+                       <div className="percentile felx">
+                         <div className="percentileresult seventyfive">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>JavaScript</p>
+                       <div className="percentile">
+                         <div className="percentileresult seventyfive">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>React.js</p>
+                       <div className="percentile">
+                         <div className="percentileresult eighty">
+
+                         </div>
+                       </div>
+                    </div>
+                   
+                    
                 </div>
                 <div>
-                    <p></p>
-                    <article></article>
+                    <h3>Back End</h3>
+                    <div className="felx">
+                       <p>Node.js </p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>Express.js</p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>Python</p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>MySQL</p>
+                       <div className="percentile">
+                         <div className="percentileresult eighty">
+
+                         </div>
+                       </div>
+                    </div>
+                
+                </div>
+                <div>
+                    <h3>Tools</h3>
+                    <div className="felx">
+                    <p> Git & GitHub</p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                    <p>  Visual Studio Code</p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                       <p>Responsive Design</p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div>
+                    <h3>Soft Skills</h3>
+                    <div className="felx">
+                    <p>Problem-solving </p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                    <p>Collabration</p> 
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                    <p>Attention to Deatail </p>
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    <div className="felx">
+                    <p>  Fast Learner</p>
+                    
+                       <div className="percentile">
+                         <div className="percentileresult">
+
+                         </div>
+                       </div>
+                    </div>
+                    
+                      
+                    
                 </div>
             </div>
      </section>
